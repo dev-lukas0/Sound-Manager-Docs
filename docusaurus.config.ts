60778592385444a8850156d466c2c489c50e1563
Lukas <r.yusifli@yahoo.com>
2025-12-14 02:56:03 +0100

    Update editUrl in docusaurus.config.ts to point to the main branch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/dev-lukas0/Sound-Manager-Docs',
+            'https://github.com/dev-lukas0/Sound-Manager-Docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
